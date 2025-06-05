341efa67d8bd4e636ee54864dbaf343e45037e85
aldanadelviso <168225894+aldanadelviso@users.noreply.github.com>
2025-06-05 00:30:17 -0300

    Utilizamos objetos y agregamos sonido

---
+++ tp1/Obra.js
@@ -0,0 +1,77 @@
+class Obra {
+  constructor(anchoBase, altoBase, espacio, gotasImagenes) {
+    this.anchoBase = anchoBase;
+    this.altoBase = altoBase;
+    this.espacio = espacio;
+    this.gotasImagenes = gotasImagenes;
+    this.gotas = [];
+    this.columnas = Math.floor(width / (altoBase + espacio)) + 2;
+    this.filas = Math.floor(height / (anchoBase + espacio));
+    this.inicializarGotas();
+  }
+
+  inicializarGotas() {
+    for (let fila = 0; fila < this.filas; fila++) {
+      for (let columna = 0; columna < this.columnas; columna++) {
+        let x = columna * (this.anchoBase + this.espacio);
+        let y = fila * (this.altoBase + this.espacio);
+        let velocidadY = random(3, 5);
+        let indice = int(random(0, this.gotasImagenes.length));
+        let tinte = random(75, 100);
+
+        let gota = new Gota(x, y, this.anchoBase, this.altoBase, velocidadY, tinte);
+        gota.imagen = this.gotasImagenes[indice];
+        this.gotas.push(gota);
+      }
+    }
+  }
+
+  dibujarFondo() {
+    for (let y = 0; y < height; y++) {
+      let inter = map(y, 0, height, 0, 1);
+      let c = lerpColor(color(100, 0, 0), color(255, 240, 230), inter);
+      stroke(c);
+      line(0, y, width, y);
+    }
+  }
+
+  reiniciarPosicionGotas() {
+    for (let g of this.gotas) {
+      g.posicionOriginal();
+    }
+  }
+
+  dibujarGotas() {
+    for (let g of this.gotas) {
+      g.dibujar();
+    }
+  }
+
+  lluviaHaciaAbajo(intensidad) {
+    for (let g of this.gotas) {
+      g.y += g.velocidadY * intensidad;
+      if (g.y > height - g.alto) {
+        g.y = -g.alto;
+      }
+      g.dibujar();
+    }
+  }
+
+  lluviaHaciaCostado(angulo, intensidad) {
+    for (let g of this.gotas) {
+      g.x += sin(angulo) * g.velocidadY * intensidad;
+      g.y += cos(angulo) * g.velocidadY * intensidad;
+      g.y += g.velocidadY * intensidad;
+
+      if (g.y > height + 50 || g.x < -50) {
+        let offset = random(-200, width + 200);
+        g.x = offset;
+        g.y = -100;
+        g.x -= sin(angulo) * 100;
+        g.y -= cos(angulo) * 100;
+      }
+
+      g.dibujar();
+    }
+  }
+}
--- tp1/tp1.js
+++ tp1/tp1.js
@@ -1,121 +1,57 @@
-let gotas = [];
 let anchoBase = 10;
 let altoBase = 10;
 let espacio = 2;
-let lluviaActiva=false;
 let gotasImagenes = [];
-let columnas;
-let filas;
 let tiempoInicio;
-let tiempoLimiteIzq = 5000; 
-let tiempoLimiteDer = 10000; 
+let tiempoLimiteIzq = 3000;
+let tiempoLimiteDer = 6000;
+let obra;
+let mic;
+let umbral = 0.01;
 
 function preload() {
   for (let i = 0; i < 4; i++) {
-    gotasImagenes[i]= loadImage("imagenes/trazo0"+i+".png");
+    gotasImagenes[i] = loadImage("imagenes/trazo0" + i + ".png");
   }
 }
 
 function setup() {
   createCanvas(400, 500);
-  columnas = Math.floor(width / (altoBase + espacio)) +2;
-  filas = Math.floor(height / (anchoBase + espacio));
   imageMode(CENTER);
-  inicializarGotas();
+  mic = new p5.AudioIn();
+  mic.start();
+  obra = new Obra(anchoBase, altoBase, espacio, gotasImagenes);
 }
 
 function draw() {
-  dibujarFondo();
-  noStroke();
-  if (mouseIsPressed) {
-    lluviaActiva=true;
-    if (!tiempoInicio){
-      tiempoInicio= millis();
-    }
-  } else {
-    lluviaActiva=false;
-    tiempoInicio=null;
-    reiniciarPosicionGotas();
-  }
+  let volumen = mic.getLevel() * 10;
+  let lluviaActiva = volumen > umbral;
 
-  if (lluviaActiva){
+  obra.dibujarFondo();
+  /*
+  fill(0);
+  noStroke();
+  rect(10, height - 30, 150, 20);
+  fill(255);
+  textSize(12);
+  text("Volumen: " + nf(volumen, 1, 4), 15, height - 15);*/
+  if (lluviaActiva) {
+    if (!tiempoInicio) {
+      tiempoInicio = millis();
+    }
+    let intensidad = map(volumen, umbral, 0.3, 2, 5, true);
+
     let tiempoTranscurrido = millis() - tiempoInicio;
     if (tiempoTranscurrido < tiempoLimiteIzq) {
-      dibujarLluviaHaciaAbajo();
-    }else if (tiempoTranscurrido < tiempoLimiteDer) {
-      dibujarLluviaHaciaCostado(PI / 10);
+      obra.lluviaHaciaAbajo(intensidad);
+    } else if (tiempoTranscurrido < tiempoLimiteDer) {
+      obra.lluviaHaciaCostado(PI / 10, intensidad);
     } else {
-      dibujarLluviaHaciaCostado(-PI / 10);
+      obra.lluviaHaciaCostado(-PI / 10, intensidad);
     }
-  }else{
-    dibujarGotas();
+  } else {
+    tiempoInicio = null;
+    obra.reiniciarPosicionGotas();
+    obra.dibujarGotas();
   }
-
-}
-
-//Funcion para crear los gotas y guardarlos en el arreglo para luego dibujarlos.
-function inicializarGotas() {
-  for (let fila = 0; fila < filas; fila++) {
-    for (let columna = 0; columna < columnas; columna++) {
-      let x = columna * (anchoBase + espacio);
-      let y = fila * (altoBase + espacio);
-      let velocidadY = random(3, 5);
-      let indice = int(random(0, 3));
-      let tinte = random(75, 100);
-
-      let gota = new Gota(x, y, anchoBase, altoBase, velocidadY, tinte);
-      gota.imagen = gotasImagenes[indice];
-      gotas.push(gota); 
-    }
-  }
-}
-
-function reiniciarPosicionGotas() {
-  for (let g of gotas) {
-    g.posicionOriginal();
-  }
-}
-
-//Funcion para pintar el fondo degradado
-function dibujarFondo(){
-  for (let y = 0; y < height; y++) {
-    let interpolacion = map(y, 0, height, 0, 1);
-    let colorFondo = lerpColor(color(100, 0, 0), color(255, 240, 230), interpolacion);
-    stroke(colorFondo);
-    line(0, y, width, y);
-  }
-}
-
-//Funcion para hacer aparecer los gotas
-function dibujarGotas(){
-  for (let g of gotas) {
-    g.dibujar();
-  }
-}
-
-
-function dibujarLluviaHaciaAbajo(){
-  for (let g of gotas) {
-    g.y += g.velocidadY;
-    if (g.y > height -g.alto) {
-      g.y = -g.alto;
-    }
-    g.dibujar();
-  }
-}
-
-function dibujarLluviaHaciaCostado(angulo){
-  for (let g of gotas) {
-      g.x += sin(angulo) * g.velocidadY;
-      g.y += cos(angulo) * g.velocidadY;
-      g.y += g.velocidadY;
-      if (g.y > height + 50 || g.x < -50) {
-          let offset = random(-200, width + 200);
-          g.x = offset;
-          g.y = -100;
-          g.x -= sin(angulo) * 100;
-          g.y -= cos(angulo) * 100;
-      }
-      g.dibujar();
-    }
 }
